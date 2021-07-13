8974568b893ad161f589c531eb8cc9e24ca4ab66
Anibal <anibal.gatica@mail.udp.cl>
2021-07-13 12:34:06 +0100

    day 3#

---
+++ notes-app/app.js
@@ -0,0 +1,7 @@
+const getNotes = require('./notes.js');
+
+// const add = require('./utils.js')
+// const sum = add(4, -2);
+// console.log(sum);
+const msg = getNotes()
+console.log(msg);
---
+++ notes-app/notes.js
@@ -0,0 +1,7 @@
+const notes = require('./notes.txt');
+
+function getNotes(){
+    return "your notes..."
+}
+
+module.exports = getNotes
