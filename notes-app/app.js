e9cb318260ddb221661c7466be99d48850f34681
Anibal <anibal.gatica@mail.udp.cl>
2021-07-20 18:20:13 +0100

    json files review

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,47 @@
+const chalk =  require('chalk');
+const yargs = require('yargs');
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
 const getNotes = require('./notes.js');
 
-// const add = require('./utils.js')
-// const sum = add(4, -2);
-// console.log(sum);
-const msg = getNotes()
-console.log(msg);
+yargs.command({
+    command: 'add',
+    describe: 'Adds a new note',
+    // showInHelp: true,
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log("Title: ", argv.title)
+        console.log("Body: ", argv.body)
+    }
+}).command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log("Removing a note")
+    }
+}).command({
+    command: 'list',
+    describe: 'List the commands',
+    handler: function () {
+        console.log("Listing the commands")
+    }
+}).command({
+    command: 'read',
+    describe: 'Read the commands',
+    handler: function () {
+        console.log("Reading the commands")
+    }
+})
+
+yargs.parse()
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,7 +1,3 @@
-const notes = require('./notes.txt');
-
 function getNotes(){
     return "your notes..."
 }
-
-module.exports = getNotes
